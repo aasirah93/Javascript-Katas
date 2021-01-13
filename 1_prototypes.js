f84d9fda145beb09f05fdbf671b88f4bacbc17e3
Asifur Rahman <aasirah93@hotmail.co.uk>
2021-01-13 20:18:50 +0000

    Constructors and Prototypes console values

--- 1_prototypes.js
+++ 1_prototypes.js
@@ -1,5 +1,22 @@
+// Constructor
 function Book(title, author, year) {
     // keys = values;
     this.title = title;
     this.author = author;
-}
+    this.year = year;
+};
+
+// getSummary
+
+Book.prototype.getSummary = function() {
+    return `${this.title} is written by ${this.author} in ${this.year}`
+};
+
+//Instantiate an object
+
+const book1 = new Book('Book One', 'Jane Doe', 2021)
+
+console.log(book1.year)
+console.log(book1)
+console.log(Object.keys(book1));
+console.log(book1.getSummary());
