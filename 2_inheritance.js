920d0fd9b67b62141b930aec4dad5131a2357041
Asifur Rahman <aasirah93@hotmail.co.uk>
2021-01-17 23:48:07 +0000

    Inheriting Javascript Prototypes

--- 2_inheritance.js
+++ 2_inheritance.js
@@ -13,6 +13,18 @@ function Magazine(title, author, year, month) {
     this.month = month;
 }
 
+// Prototype
+// getSummary
+
+Book.prototype.getSummary = function() {
+    return `${this.title} is written by ${this.author} in ${this.year}`
+};
+
+//Inherit Prtototype
+Magazine.prototype = Object.create(Book.prototype);
+
 // Instantiate Mag Object
 
 const mag1 = new Magazine('Mag One, John Dough', '2018', 'Jan');
+
+console.log(mag1)
